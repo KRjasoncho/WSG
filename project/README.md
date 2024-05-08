3ae56f4eaf09e46c1e5dcecfeab9b12c63255a57
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-05-08 22:51:52 +0900

    header.svelte html,css완성

---
+++ project/README.md
@@ -0,0 +1,1 @@
+_word search game_
---
+++ project/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import home from "./pages/home.svelte";
+  import Router from "svelte-spa-router";
+
+  const routes = {
+    "/": home,
+  };
+</script>
+
+<Router {routes} />
